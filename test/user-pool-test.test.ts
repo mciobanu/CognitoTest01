8519efd8c29182bf80c09bf09b230d58062e2bc6
ciobi <ciobi07@users.sourceforge.net>
2022-01-19 21:59:37 +0200

    Initial commit

---
+++ test/user-pool-test.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as UserPoolTest from '../lib/user-pool-test-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/user-pool-test-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new UserPoolTest.UserPoolTestStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
